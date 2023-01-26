057aec99770f9c51be8ace3632e501e677b413ba
Gerardo <gerardo.aguilar01@outlook.com>
2023-01-25 22:44:00 -0600

    Typed fetchBreedList

---
+++ src/fetchBreedList.ts
@@ -0,0 +1,19 @@
+import { QueryFunction } from "@tanstack/react-query";
+import { Animal, BreedListApiResponse } from "./ApiResponsesTypes";
+
+const fetchBreedList: QueryFunction<BreedListApiResponse, ["breeds", Animal]> =
+  async ({ queryKey }) => {
+    const animal = queryKey[1];
+
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    );
+
+    if (!res.ok) {
+      throw new Error(`breeds ${animal} fetch not ok`);
+    }
+
+    return res.json();
+  }
+
+export default fetchBreedList;
